9e2e1b1fc2d7ddcb845ec7a78207c17b0dfa63e3
Marcus Hammarberg <marcusoft.net@gmail.com>
2012-11-19 20:58:49 +0100

    Proxy property on Deviation model class to handle TimeSpan persistance

---
+++ Aptitud.DeviationReporter.UnitTests/TimeSpanTrickeryUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Models;
+using NUnit.Framework;
+using Should.Fluent;
+
+namespace Aptitud.DeviationReporter.UnitTests
+{
+    [TestFixture]
+    public class TimeSpanTrickeryUnitTests
+    {
+        [Test]
+        public void Duration_should_set_DurationTicks()
+        {
+            var d = new Deviation { Duration = TimeSpan.FromTicks(100) };
+
+            d.DurationTicks.Should().Equal((Int64)100);
+        }
+
+        [Test]
+        public void setting_DurationTicks_should_read_same_from_Duration()
+        {
+            var ts =  TimeSpan.FromTicks(100);
+            var d = new Deviation { DurationTicks = 100 };
+
+            d.Duration.Should().Equal(ts);
+        }
+    }
+}
