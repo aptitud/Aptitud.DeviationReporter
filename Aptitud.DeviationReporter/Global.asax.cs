6727cce80f3b0ad6fdd5ead3b7319ed193292801
marcusoftnet <marcusoft.net@gmail.com>
2012-11-26 19:58:12 +0100

    Hacked the IoC since WebApi and Ninject doesn't play nice together

--- Aptitud.DeviationReporter/Global.asax.cs
+++ Aptitud.DeviationReporter/Global.asax.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿
+
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-
+using Simple.Data;
 namespace Aptitud.DeviationReporter
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -24,6 +18,11 @@ namespace Aptitud.DeviationReporter
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        
+        public static dynamic DB
+        {
+            get { return Database.Open(); }
         }
     }
 }
